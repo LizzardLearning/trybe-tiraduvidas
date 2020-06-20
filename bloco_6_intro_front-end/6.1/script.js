3a1d0961600e5d71f182699a40780aecf0aeac72
Isabela Rosa <isabelarosa954@gmail.com>
2020-06-20 14:07:53 -0300

    dúvida sobre funções simples, arrow functions e testes unitários

---
+++ bloco_6_intro_front-end/6.1/script.js
@@ -0,0 +1,7 @@
+var estados =document.getElementById('estados-br');
+
+http://devfuria.com.br/javascript/dom-create-element/
+
+http://devfuria.com.br/javascript/manipulando-combobox-select-com-javascript/
+
+https://www.freecodecamp.org/news/a-step-by-step-guide-to-getting-started-with-html-forms-7f77ae4522b5/
